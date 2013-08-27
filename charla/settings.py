4919edc3c197c2901fd6f8b5d8ad4f1a962114a6
Israel Fermin Montilla <israel@ringtu.com>
2013-08-27 15:15:46 -0430

    Se configura la base de datos con postgresql

--- charla/settings.py
+++ charla/settings.py
@@ -15,11 +15,11 @@ MANAGERS = ADMINS
 if 'test' in sys.argv:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': ':memory:',                      # Or path to database file if using sqlite3.
+            'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': 'charla',                      # Or path to database file if using sqlite3.
             # The following settings are not used with sqlite3:
-            'USER': '',
-            'PASSWORD': '',
+            'USER': 'django',
+            'PASSWORD': 'djangouser',
             'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
             'PORT': '',                      # Set to empty string for default.
         }
